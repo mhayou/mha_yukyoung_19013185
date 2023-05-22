2b596503694ec33a9b8555cc1cac8dd017580659
hello <hello@DESKTOP-CD9F8EM>
2023-05-22 23:07:11 +0900

    test_1

--- test_1.m
+++ test_1.m
@@ -5,7 +5,7 @@ function ECI=PQW2ECI(arg_prg,inc_angle,RAAN)
 
 R1=[cos(RAAN) -sin(RAAN) 0;sin(RAAN) cos(RAAN) 0;0 0 1];
 R2=[1 0 0;0 cos(inc_angle) -sin(inc_angle);0 sin(inc_angle) cos(inc_angle)];
-R3=[cos(w) -sin(arg_prg) 0;sin(arg_prg) cos(arg_prg) 0;0 0 1];
+R3=[cos(arg_prg) -sin(arg_prg) 0;sin(arg_prg) cos(arg_prg) 0;0 0 1];
 
 ECI=R1*R2*R3;
 end
