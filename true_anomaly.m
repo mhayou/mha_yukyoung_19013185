9aceff05fb57eaffe82eb75351c97b72e5e5b860
rina0_x8hbzy8 <rina0_x8hbzy8@DESKTOP-9L3P7MR>
2023-06-22 23:31:15 +0900

    new

--- true_anomaly.m
+++ true_anomaly.m
@@ -3,17 +3,17 @@ function nu = true_anomaly(a, e, t, toc, M0)
 
 mu = 398600.4418; 
 
-dt = datetime(t) ;
+dt = datetime(t);
 
 [h, m, s] = hms(dt);
 
-d_min = h*60 + m + s/60;
+d_mins = h*60 + m + s/60;
 
 % iteration calcutaion of E
 
-n = sqrt(mu/(a^3));
+n = sqrt(mu/(a^3))*60;
 
-M = n*d_min + M0*pi/180;
+M = n*d_mins + M0*pi/180;
 E = M;
 
 while(1)
@@ -32,7 +32,5 @@ nu = 2*atan2(tan(E/2)*sqrt(1+e),sqrt(1-e));
 if nu<0
     nu = nu+2*pi;
 end
-
-nu = nu*(180/pi);
 
 end
